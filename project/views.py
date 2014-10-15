c8e17642b0093d12402b32f677e6f37842d8f044
Pratik Kulkarni <prat8897@gmail.com>
2014-10-15 12:39:54 +0530

    Final Draft. Phew!
    Only thing left now is to change the posts and make your own.

--- project/views.py
+++ project/views.py
@@ -1,15 +1,17 @@
 from flask import render_template
-
+from flask import request
 from app import app, pages
 
 
 @app.route('/')
 def home():
+    
     posts = [page for page in pages]
     return render_template('page.html', page=posts[0])
 
 @app.route('/archive/')
 def archive():
+    
     posts = [page for page in pages if 'date' in page.meta]
     # Sort pages by date
     sorted_posts = sorted(posts, reverse=True,
@@ -18,14 +20,17 @@ def archive():
 
 @app.route('/comics/')
 def comics():
+    
     return render_template('comics.html', page=page)
 
 @app.route('/about/')
 def about():
+    
     return render_template('about.html', page=page)
 
 @app.route('/<path:path>/')
 def page(path):
+    
     # Path is the filename of a page, without the file extension
     # e.g. "first-post"
     page = pages.get_or_404(path)
@@ -33,4 +38,5 @@ def page(path):
 
 @app.errorhandler(404)
 def page_not_found(e):
+    
     return render_template('404.html'), 404
